a9d4a9479369572e99317fca62d22610883d29a1
oyesa <mercyoyesa2018@gmail.com>
2022-04-15 10:26:57 +0300

    Add navbar, footer, repo and profile components

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { RepoComponent } from './repo/repo.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    RepoComponent,
+    ProfileCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
