7e0b55e02a5f93e27e5366cfcfac660efe73d921
oyesa <mercyoyesa2018@gmail.com>
2022-04-15 17:03:17 +0300

    import profile service to repo component

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-repo',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo.component.css']
 })
 export class RepoComponent implements OnInit {
+  Repos: any;
+  profile: any;
 
-  constructor() { }
+  constructor( private profileServce: ProfileService) { 
+    this.profileServce = profileServce;
+  }
+
+  getProfileRepos(){}
 
   ngOnInit(): void {
+    this.profileServce.getProfileRepos().subscribe((Repos: any[]) =>{
+      console.log(Repos);
+      this.Repos = Repos;
+    });
   }
 
 }
