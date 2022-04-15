108a37692d96cd08a12be2cc740f3868e3d15e1c
oyesa <mercyoyesa2018@gmail.com>
2022-04-15 16:48:28 +0300

    add function to get data from api

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username: string;
+  private clientid = '';
+  private clientsecret = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    console.log('Okay');
+    this.username = 'Oyesa';
+  }
+
+  getdata(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+  }
+
+  getProfileRepo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+  }
+
+  updateProfile(username: string){
+    this.username =  this.username;
+  }
 }
